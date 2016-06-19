2e59bbc276360c4e7341db30c9e8c8b0709fa801
seogi1004 <seogi777@gmail.com>
2016-06-20 00:48:24 +0900

    에러 찾기 샘플 작성 중...  - 5.14.5까지 했음

--- alvin/finderror/task.py
+++ alvin/finderror/task.py
@@ -1,7 +1,11 @@
 from alvin.util.GF2 import one
 from alvin.util.matutil import listlist2mat
 from alvin.util.vecutil import list2vec
-
+from alvin.util.bitutil import str2bits
+from alvin.util.bitutil import bits2str
+from alvin.util.bitutil import bits2mat
+from alvin.util.bitutil import mat2bits
+from alvin.util.bitutil import noise
 
 # Task 5.14.1
 G = listlist2mat([
@@ -63,6 +67,9 @@ e2 = find_error(list2vec([ one, 0, one, one, 0, one, 0 ]))
 print(e2)
 print("\n")
 
+
+# Task 5.14.5
+print("Task 5.14.5")
 def find_error_matrix(e):
     index = 0
     for i in range(3):
@@ -72,3 +79,28 @@ def find_error_matrix(e):
 
 print(find_error_matrix(e1))
 print(find_error_matrix(e2))
+print("\n")
+
+
+# Task 5.14.6
+print("Task 5.14.6")
+s1 = "Alvin Hong"
+s2 = str2bits(s1)
+print(s1)
+print(s2)
+print(bits2str(s2))
+print("\n")
+
+
+# Task 5.14.7
+print("Task 5.14.7")
+P = bits2mat(s2)
+print(P)
+print("\n")
+
+
+# Task 5.14.9
+print("Task 5.14.9")
+E = noise(P, 0.02)
+print(bits2str(mat2bits(E + P)))
+print("\n")
