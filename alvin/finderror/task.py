dfc365d467d66f916e6ba8c187e7c03c2da9ff3c
seogi1004 <seogi777@gmail.com>
2016-06-21 01:23:26 +0900

    에러 찾기 샘플 작성 중...  - 5.14.5까지 했음

--- alvin/finderror/task.py
+++ alvin/finderror/task.py
@@ -1,5 +1,6 @@
 from alvin.util.GF2 import one
 from alvin.util.matutil import listlist2mat
+from alvin.util.matutil import mat2coldict
 from alvin.util.vecutil import list2vec
 from alvin.util.bitutil import str2bits
 from alvin.util.bitutil import bits2str
@@ -26,6 +27,7 @@ p = list2vec([
 c = G * p
 
 print("Task 5.14.1")
+print(G)
 print(c)
 print("\n")
 
@@ -39,6 +41,7 @@ R = listlist2mat([
 ])
 
 print("Task 5.14.2")
+print(R)
 print(R * c)
 print(R * G)
 print("\n")
@@ -64,6 +67,15 @@ print("Task 5.14.4")
 e1 = find_error(list2vec([ one, 0, one, one, 0, one, one ]))
 print(e1)
 e2 = find_error(list2vec([ one, 0, one, one, 0, one, 0 ]))
+#e2 = find_error(listlist2mat([
+#    [one],
+#    [0],
+#    [one],
+#    [one],
+#    [0],
+#    [one],
+#    [0]
+#]))
 print(e2)
 print("\n")
 
@@ -84,10 +96,11 @@ print("\n")
 
 # Task 5.14.6
 print("Task 5.14.6")
-s1 = "Alvin Hong"
+s1 = "I'm trying to free your mind, Neo. But I can only show you the door. You're the one that has to walk through it."
 s2 = str2bits(s1)
 print(s1)
 print(s2)
+print(bits2mat(s2))
 print(bits2str(s2))
 print("\n")
 
@@ -103,4 +116,66 @@ print("\n")
 print("Task 5.14.9")
 E = noise(P, 0.02)
 print(bits2str(mat2bits(E + P)))
+print("\n")
+
+
+# Task 5.14.10
+print("Task 5.14.10")
+print(P)
+C = G * P
+print(bits2str(mat2bits(C)))
+print("\n")
+
+
+# Task 5.14.11
+print("Task 5.14.11")
+E = noise(C, 0.02)
+CTILDE = E + C
+print(bits2str(mat2bits(CTILDE)))
+# 복호화 노이즈
+print(bits2str(mat2bits(R * CTILDE)))
+# 복호화 원문
+print(bits2str(mat2bits(R * C)))
+print("\n")
+
+
+# Task 5.14.12
+print("Task 5.14.12")
+print(C)
+print(CTILDE)
+print("======================\n")
+#print(R * CTILDE)
+#print(bits2str(mat2bits(R * CTILDE)))
+#print("----------------------\n")
+print(find_error(C))
+print(find_error(CTILDE))
+#print("----------------------\n")
+print("\n")
+
+
+# Task 5.14.13
+print("Task 5.14.13")
+def correct(c):
+    print("Before: " + bits2str(mat2bits(R * c)))
+
+    errordict = mat2coldict(find_error(c))
+    size = len(errordict)
+
+    # 영행렬 생성
+    E = c.copy()
+    for j in range(size):
+        for i in range(7):
+            E[i,j] = 0
+
+    # 에러 신드롬 생성
+    for j in range(size):
+        i = find_error_matrix(errordict[j])
+        if i > 0:
+            E[i - 1, j] = one
+
+    result = c + E
+
+    print("After: " + bits2str(mat2bits(R * result)))
+
+correct(CTILDE)
 print("\n")
